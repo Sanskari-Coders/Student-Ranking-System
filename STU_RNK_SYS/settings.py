4fac0e851c3a63b7cfa491fae391537f8b370ef3
Anshuman Rath <anshumanrath030@gmail.com>
2024-03-17 20:23:20 +0530

    .

--- STU_RNK_SYS/settings.py
+++ STU_RNK_SYS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n0&o@ce79xb-7je)^0j^!#8@3pzid^la@0mardzs@cspgtqv9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','*.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
