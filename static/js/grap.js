cbff19201b05a3195fc25de230f2b142ddce45ee
Adarsh Nanda <adarshananda51@gmail.com>
2024-04-07 13:01:14 +0530

    add css

--- static/js/grap.js
+++ static/js/grap.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['IT', 'BCA', 'MATH', 'phy', 'Chem','zoolgy','bot'],
+        labels: ['IT', 'BCA', 'MATH', 'Zoo', 'Bot','Phy','Chem'],
         datasets: [{
           label: 'hide',
           data: [8.5,4,7,9,5,6.5,9, 10],
           backgroundColor: [
             'rgb(91, 97, 217)',
             'rgb(218, 223, 255)',
-            'rgb(220, 24, 84)',
-            'rgba(106,77,170)',
+            'rgb(75, 184, 189)',
+            'rgba(220, 24, 84)',
+            'rgb(104, 174, 30)',
+            'rgb(106,77,170)',
             'rgb(253, 135, 6)',
-            'rgb(205,255,143)',
-            'rgb(206, 253, 255)'
             
           ],
           borderWidth: 1
