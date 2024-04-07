aec4e7fbbcc1e6a23a4c1c0bdef9958c1e8a51cd
Anshuman Rath <anshumanrath030@gmail.com>
2024-04-07 13:29:14 +0530

    linked problem solve

--- departmentwiseview/urls.py
+++ departmentwiseview/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from departmentwiseview import views
 urlpatterns = [
-    path('BCA/'     ,views.BCA    ,name='BCA'),
-    path('BSC(IT)/' ,views.BSC_IT ,name='BSC(IT)'),
+    path('BCA|IT/'  ,views.BCAIT    ,name='BCA|IT'),
     path('BSC(PHY)/',views.BSC_PHY,name='BSC(PHY)'),
     path('BSC(CHE)/',views.BSC_CHE,name='BSC(CHE)'),
     path('BSC(MAT)/',views.BSC_MAT,name='BSC(MAT)'),
--- departmentwiseview/views.py
+++ departmentwiseview/views.py
@@ -3,8 +3,8 @@ from datastore.models import Student
 import pandas as pd
 
 
-def BCA(request):
-    data = Student.objects.filter(department='BCA')
+def BCAIT(request):
+    data=Student.objects.filter(department=['BCA','IT'])
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
@@ -15,16 +15,16 @@ def BCA(request):
     return render(request, 'departmentview.html', context)
 
 
-def BSC_IT(request):
-    data = Student.objects.filter(department='BSC(IT)')
-    student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
-    df = pd.DataFrame(student_data)
-    sorted_df = df.sort_values(by='CGPA', ascending=False)
-    context = {
-        'sorted_df': sorted_df,  
-    }
-    print(sorted_df)
-    return render(request, 'departmentview.html', context)
+# def BSC_IT(request):
+#     data = Student.objects.filter(department='BSC(IT)')
+#     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
+#     df = pd.DataFrame(student_data)
+#     sorted_df = df.sort_values(by='CGPA', ascending=False)
+#     context = {
+#         'sorted_df': sorted_df,  
+#     }
+#     print(sorted_df)
+#     return render(request, 'departmentview.html', context)
 
 
 def BSC_PHY(request):
