01da705dd0c9edb1b1ec2985d2629999741e1ed0
Anshuman Rath <anshumanrath030@gmail.com>
2024-04-08 16:22:19 +0530

    department khatam

--- departmentwiseview/views.py
+++ departmentwiseview/views.py
@@ -8,8 +8,11 @@ def BCAIT(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html',context=context)
@@ -32,8 +35,11 @@ def BSC_PHY(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html', context)
@@ -44,8 +50,11 @@ def BSC_CHE(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html', context)
@@ -56,8 +65,11 @@ def BSC_MAT(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html', context)
@@ -68,8 +80,11 @@ def BSC_BOT(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html', context)
@@ -79,8 +94,11 @@ def BSC_ZOO(request):
     student_data = [{'Name': student.name,'Regd_No':student.registration_number,'Department':student.department,'Semester':student.semester,'SGPA':student.sgpa,'CGPA': student.cgpa} for student in data]
     df = pd.DataFrame(student_data)
     sorted_df = df.sort_values(by='CGPA', ascending=False)
+    students_count = df.__len__()
+    
     context = {
-        'sorted_df': sorted_df,  
+        'sorted_df': sorted_df,
+        'students_count': students_count,  
     }
     print(sorted_df)
     return render(request, 'departmentview.html', context)
