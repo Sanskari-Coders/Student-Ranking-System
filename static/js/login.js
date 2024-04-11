aeec14a5da075b5e750ab9d76165c0e1d70b1e80
Adarsh Nanda <adarshananda51@gmail.com>
2024-04-11 20:50:27 +0530

    ..

--- static/js/login.js
+++ static/js/login.js
@@ -1,6 +1,17 @@
 let letsgo=document.getElementById("letsgo")
+let letsGO=document.getElementById("letsGO");
+let lodinganimation=document.getElementById("lodinganimation")
+
 letsgo.addEventListener("click",function(){
-window.location=("{% url 'HOME' %}")
+
+    letsGO.style.display="none"
+    lodinganimation.style.display="flex"
+
+
+    setTimeout(function() {
+        window.location=("https://adarshananda.com")
+       },2000);
+
 })
 
 
